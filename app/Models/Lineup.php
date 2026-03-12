afe4c9522c1748db28aaca8f8779c808df4eded0
mohamedAttefi <attefimohamed0@gmail.com>
2026-03-12 10:56:41 +0000

    add lineup model and relations

---
+++ app/Models/Lineup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lineup extends Model
+{
+    /** @use HasFactory<\Database\Factories\LineupFactory> */
+    use HasFactory;
+
+    protected $fillable = ['match_id', 'team_id'];
+
+    public function match()
+    {
+        return $this->belongsTo(GamePlay::class);
+    }
+
+    public function players()
+    {
+        return $this->belongsToMany(Player::class, 'lineup_players')
+            ->withPivot('position', 'is_starter');
+    }
+}
