0f650c78d44c0c2edc46903c341d00d4f193826e
mohamedAttefi <attefimohamed0@gmail.com>
2026-03-12 10:57:34 +0000

    add team model and relations

---
+++ app/Models/Team.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    /** @use HasFactory<\Database\Factories\TeamFactory> */
+    use HasFactory;
+    protected $fillable = ['name', 'class_group'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function homeMatches()
+    {
+        return $this->hasMany(GamePlay::class, 'home_team_id');
+    }
+
+    public function awayMatches()
+    {
+        return $this->hasMany(GamePlay::class, 'away_team_id');
+    }
+
+    public function standing()
+    {
+        return $this->hasOne(Standing::class);
+    }
+
+    public function players()
+    {
+        return $this->hasMany(Player::class);
+    }
+
+}
