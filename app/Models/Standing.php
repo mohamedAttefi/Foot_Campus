722ce7ef1abe8e22d7f8a3095ac558e88ade62b4
mohamedAttefi <attefimohamed0@gmail.com>
2026-03-12 10:57:20 +0000

    add standing model and relations

---
+++ app/Models/Standing.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Standing extends Model
+{
+    /** @use HasFactory<\Database\Factories\StandingFactory> */
+    use HasFactory;
+
+    protected $fillable = ['points', 'wins', 'draws', 'losses'];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+    
+}
