9f3158b9b8da86e9a6b38bc1b9811eeb73b91009
mohamedAttefi <attefimohamed0@gmail.com>
2026-03-25 11:39:29 +0100

    add match stats

---
+++ app/Http/Controllers/MatchStatController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MatchStat;
+use App\Http\Requests\StoreMatchStatRequest;
+use App\Http\Requests\UpdateMatchStatRequest;
+
+class MatchStatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMatchStatRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MatchStat $matchStat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMatchStatRequest $request, MatchStat $matchStat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MatchStat $matchStat)
+    {
+        //
+    }
+}
