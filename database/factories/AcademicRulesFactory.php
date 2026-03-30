46cca44c906159ecf30d6c47b4b055e47150191c
mohamedAttefi <attefimohamed0@gmail.com>
2026-03-30 12:08:56 +0100

    feat: add AcademicRulesPolicy and AcademicRulesFactory for managing academic rules permissions and data generation

---
+++ database/factories/AcademicRulesFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AcademicRules;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<AcademicRules>
+ */
+class AcademicRulesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
