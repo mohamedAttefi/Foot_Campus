1469da74b6c8812829451664219cd884140780a2
mohamedAttefi <attefimohamed0@gmail.com>
2026-03-30 12:05:52 +0100

    feat: add AcademicRulesController with CRUD methods

---
+++ app/Http/Controllers/AcademicRulesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AcademicRules;
+use App\Http\Requests\StoreAcademicRulesRequest;
+use App\Http\Requests\UpdateAcademicRulesRequest;
+
+class AcademicRulesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAcademicRulesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AcademicRules $academicRules)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAcademicRulesRequest $request, AcademicRules $academicRules)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AcademicRules $academicRules)
+    {
+        //
+    }
+}
