f2c9708ad222a8b610ba9569b3c1bafe3e74f134
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-13 11:09:17 +0100

    feat: add public consultation routes for matches, standings, and player/team statistics

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\MatchEventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Public Consultation Routes
+Route::get('/public/matches', [PublicController::class, 'matches']);
+Route::get('/public/matches/{id}', [PublicController::class, 'matchDetails']);
+Route::get('/public/standings', [PublicController::class, 'standings']);
+Route::get('/public/player-statistics', [PublicController::class, 'playerStatistics']);
+Route::get('/public/team-statistics', [PublicController::class, 'teamStatistics']);
+
+Route::resource('match-events', MatchEventController::class);
