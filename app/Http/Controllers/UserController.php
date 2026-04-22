8cc920fe82e697ebf87f4b1b5e825a79fcd375e4
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-22 12:04:53 +0100

    Remove authorization check for viewing all users in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,6 @@ class UserController extends Controller
      */
     public function index()
     {
-        $this->authorize('viewAny', User::class);
         $users = User::all();
         return response()->json($users);
     }
