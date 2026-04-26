09bb9818a4b117c6fab6d1f2e1e425b33705bf04
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-26 18:56:00 +0100

    Add validated_by_admin attribute to Lineup model and clarify relationship definitions

--- app/Models/Lineup.php
+++ app/Models/Lineup.php
@@ -10,16 +10,26 @@ class Lineup extends Model
     /** @use HasFactory<\Database\Factories\LineupFactory> */
     use HasFactory;
 
-    protected $fillable = ['game_play_id', 'team_id'];
+    protected $fillable = ['game_play_id', 'team_id', 'validated_by_admin'];
 
     public function match()
     {
-        return $this->belongsTo(GamePlay::class);
+        return $this->belongsTo(GamePlay::class, 'game_play_id');
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
     }
 
     public function players()
     {
         return $this->belongsToMany(Player::class, 'lineup_players')
             ->withPivot('position', 'is_starter');
+    }
+
+    public function lineupPlayers()
+    {
+        return $this->hasMany(LineupPlayer::class);
     }
 }
