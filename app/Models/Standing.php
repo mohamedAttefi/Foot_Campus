5e010d9875022629a985e5a9e98547d64e79d397
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-26 18:56:54 +0100

    Add team_id to fillable attributes in Standing model

--- app/Models/Standing.php
+++ app/Models/Standing.php
@@ -10,7 +10,7 @@ class Standing extends Model
     /** @use HasFactory<\Database\Factories\StandingFactory> */
     use HasFactory;
 
-    protected $fillable = ['points', 'wins', 'draws', 'losses', 'season_id', 'goals_for', 'goals_against', 'goal_difference'];
+    protected $fillable = ['team_id', 'points', 'wins', 'draws', 'losses', 'season_id', 'goals_for', 'goals_against', 'goal_difference'];
 
     public function team()
     {
