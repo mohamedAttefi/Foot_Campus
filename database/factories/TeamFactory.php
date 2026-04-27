9756e19c171d836f911bbdeaa25b4e45b271d0a8
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-27 19:55:41 +0100

    Remove default attributes from TeamFactory definition

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -17,8 +17,7 @@ class TeamFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->company(),
-            'city' => fake()->city(),
+            
         ];
     }
 }
