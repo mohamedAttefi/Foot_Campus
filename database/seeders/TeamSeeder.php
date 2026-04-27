42dcab8e5e3a1b01a3b1e7a53ee0f05234467d83
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-27 20:12:55 +0100

    Add TeamSeeder to populate teams with coach assignment

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,24 @@ class TeamSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Get the coach user for team assignment
+        $coach = User::where('email', 'coach@campus.com')->first();
+
+        $teams = [
+            [
+                'name' => 'Campus Eagles',
+                'class_group' => 'Senior',
+                'coach_id' => $coach ? $coach->id : 1,
+            ],
+            [
+                'name' => 'Academic Lions',
+                'class_group' => 'Junior',
+                'coach_id' => $coach ? $coach->id : 1,
+            ],
+        ];
+
+        foreach ($teams as $team) {
+            Team::create($team);
+        }
     }
 }
