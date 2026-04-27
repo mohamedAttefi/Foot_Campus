9613bf581c6bade5b745a0dba2f21279186f70ac
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-27 19:56:36 +0100

    Add AcademicRules seeder to populate academic rules data

--- database/seeders/AcademicRulesSeeder.php
+++ database/seeders/AcademicRulesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AcademicRules;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class AcademicRulesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $rules = [
+            [
+                'min_average_threshold' => 2.50,
+                'max_failed_subjects' => 2,
+                'is_active' => true,
+            ],
+            [
+                'min_average_threshold' => 3.50,
+                'max_failed_subjects' => 1,
+                'is_active' => true,
+            ],
+            [
+                'min_average_threshold' => 2.00,
+                'max_failed_subjects' => 3,
+                'is_active' => true,
+            ],
+        ];
+
+        foreach ($rules as $rule) {
+            AcademicRules::create($rule);
+        }
     }
 }
