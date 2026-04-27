151d76b2957a2d607563bf876e9753ce58d0c0cc
mohamedAttefi <attefimohamed0@gmail.com>
2026-04-27 20:00:54 +0100

    Add LineupPlayerSeeder to populate player lineups with positions and starter status

--- database/seeders/LineupPlayerSeeder.php
+++ database/seeders/LineupPlayerSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\LineupPlayer;
+use App\Models\Lineup;
+use App\Models\Player;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,29 @@ class LineupPlayerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Get all lineups
+        $lineups = Lineup::all();
+        
+        foreach ($lineups as $lineup) {
+            // Get players for this team
+            $teamPlayers = Player::where('team_id', $lineup->team_id)->take(15)->get();
+            
+            // Define positions (11 starters + 4 substitutes)
+            $positions = [
+                'GK', 'DEF', 'DEF', 'DEF', 'DEF', 
+                'MID', 'MID', 'MID', 'MID', 
+                'ST', 'ST', 
+                'SUB', 'SUB', 'SUB', 'SUB'
+            ];
+            
+            foreach ($teamPlayers as $index => $player) {
+                LineupPlayer::create([
+                    'lineup_id' => $lineup->id,
+                    'player_id' => $player->id,
+                    'position' => $positions[$index] ?? 'SUB',
+                    'is_starter' => $index < 11,
+                ]);
+            }
+        }
     }
 }
